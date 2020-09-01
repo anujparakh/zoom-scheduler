ed27cf08d3f4f0edb65425e9a86e17e26db2f696
Anuj Parakh <anuj@anujinfotech.com>
2020-09-01 12:26:29 -0500

    updated to use cron to fix scheduling bugs

--- waiter.js
+++ waiter.js
@@ -1,5 +1,6 @@
 const searchClassFunction = require('./searcher').searchAndOpenClass
-var schedule = require('node-schedule')
+// var schedule = require('node-schedule')
+var CronJob = require('cron').CronJob;
 const moment = require('moment')
 
 const classes = require('./data.json').classes;
@@ -16,21 +17,32 @@ function setSchedulerForClasses()
         {
             days.push(parseInt(moment(day, 'dddd').format('d')))
         }
-        console.log('Days: ' + aClass.days)
+        console.log('Days: ' + days)
         // Get the time
         const classTime = moment(aClass.time, "hh:mm A")
         console.log('Class is at: ' + classTime.format('hh:mm A'))
 
-        // Set the schedule
-        var rule = new schedule.RecurrenceRule();
-        rule.dayOfWeek = days
-        rule.hour = parseInt(classTime.subtract(2, 'minutes').format('HH'))
-        rule.minute = parseInt(classTime.subtract(2, 'minutes').format('mm'))
+        //
+        // Set the schedule using node-scheduler
+        //
+        // var rule = new schedule.RecurrenceRule();
+        // rule.dayOfWeek = days
+        // rule.hour = parseInt(classTime.format('HH'))
+        // rule.minute = parseInt(classTime.subtract(2, 'minutes').format('mm'))
+        // var job = schedule.scheduleJob(rule, searchClassFunction)
 
-        var job = schedule.scheduleJob(rule, searchClassFunction)
+        //
+        // Set the schedule using node-cron
+        //
+        cronString = classTime.subtract(2, 'minutes').format('mm') + ' '
+            + classTime.format('HH') + ' '
+            + '* * ' + days
+        var job = new CronJob(cronString, searchClassFunction, null, true);
+        console.log(job.nextDates().format('llll'))
     }
 
-    console.log('\nScheduling Done!\n')
+    // console.log('\nScheduling Done! (using node-scheduler) \n')
+    console.log('\nScheduling Done! (using cron) \n')
 
     searchClassFunction()
 }
